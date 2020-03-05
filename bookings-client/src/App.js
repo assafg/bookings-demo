e279ab5b6d77d19119e3d49deeb4954df65b79ee
assafg <assaf@tikalk.com>
2020-03-05 17:16:35 +0200

    Update App.js

--- bookings-client/src/App.js
+++ bookings-client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     return (
         <div className="App">
             <ApiProvider>
+                <h1>Ecme Bookings</h1>
                 <Router>
                     <Switch>
                         <Route path="/listing/:id" component={Listing} />
