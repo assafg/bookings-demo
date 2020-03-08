240aeed1a96e08ca871027a3b4d78a58e61ec55c
liya <liya@tikalk.com>
2020-03-08 17:13:01 +0200

    fix build

--- build.sh
+++ build.sh
@@ -11,36 +11,44 @@ export BOOKING_DEMO_VERSION=${BASE_VERSION}.${BUILD_NUMBER}
 echo "Build bookings-client"
 cd bookings-client
 docker build -t ${DOCKER_REGISTRY}/tikalk/bookings-client:${BOOKING_DEMO_VERSION} .
-echo "Run test"
+echo "Run unit tests"
 docker run --rm -e CI=true --name bookings-client-test ${DOCKER_REGISTRY}/tikalk/bookings-client:${BOOKING_DEMO_VERSION} yarn test
 cd -
 
 echo "Build listings-gql"
 cd listings-gql
 docker build -t ${DOCKER_REGISTRY}/tikalk/listings-gql:${BOOKING_DEMO_VERSION} .
-echo "Run test"
+echo "Run unit tests"
 docker run --rm -e CI=true --name listings-gql-test ${DOCKER_REGISTRY}/tikalk/listings-gql:${BOOKING_DEMO_VERSION} yarn test
 cd -
 
 echo "Build listings-service"
 cd listings-service
 docker build -t ${DOCKER_REGISTRY}/tikalk/listings-service:${BOOKING_DEMO_VERSION} .
-echo "Run test"
-docker run --rm -e CI=true --name listings-service ${DOCKER_REGISTRY}/tikalk/listings-servicel:${BOOKING_DEMO_VERSION} yarn test
+echo "Run unit tests"
+docker run --rm -e CI=true --name listings-service ${DOCKER_REGISTRY}/tikalk/listings-service:${BOOKING_DEMO_VERSION} yarn test
 cd -
+
+echo "Get demo data"
+wget https://tikal-academy-mongo-seed-data.s3-eu-west-1.amazonaws.com/backup_data.tgz
+tar xvfz backup_data.tgz
 
 echo "Run integration test"
 # TODO update docker-compose
 docker-compose up -d
+sleep 20
 
-INTTEST=`curl -I -s http://localhost:5000/\#/0 | grep "200 OK" | `
+# echo "Load data"
+docker exec bookings-demo_mongo_1 mongorestore --gzip /var/backup
+
+INTTEST=`curl -I -s http://localhost:5000/\#/0 | grep "200 OK" | wc -l`
 
 docker-compose stop
 docker-compose rm -f
 
 echo $INTTEST
 
-if [[ "${INTTEST}" != "HTTP\/1.1 200 OK" ]]
+if [[ "${INTTEST}" != "1" ]]
 then
   echo "The test has failed!!"
   exit 1
